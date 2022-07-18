b9c53c4ea416362c32cf497bb936fdd71f64459f
이혜진 <annie_1229@naver.com>
2022-07-18 16:45:50 +0900

    test next-auth

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useSession, signIn, signOut } from "next-auth/react"
 import Image from 'next/image'
 import { hasCookie } from 'cookies-next';
 import styles from '../styles/components/header.module.scss';
 
 export default function Header({ label, onClickBtn }) {
   const router = useRouter();
+  const { session } = useSession();
   const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
@@ -40,13 +42,26 @@ export default function Header({ label, onClickBtn }) {
         <div className={styles.headerTitle} onClick={goToLobby}>{`{ CODE: ‘뚝딱’ }`}</div>
       </div>
       <div className={styles.headerRow}>
-      {
+      {/* {
         isLogin
         ? <div className={styles.myPageBtn} onClick={onClickBtn}>{label}</div>
         : <div className={styles.loginBtn}  onClick={login}>
             <Image src="/github.png" alt="github Logo" width={20} height={20} />
             <div className={styles.loginText}>로그인</div>
           </div>
+      } */}
+      {
+        session
+        ?
+            <>
+              Signed in as {session.user.email} <br />
+              <button onClick={() => signOut()}>Sign out</button>
+            </>
+        :
+          <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+          </>
       }
       </div>
     </>
