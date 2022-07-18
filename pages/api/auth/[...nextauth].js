ef2c1abe2199d5933eed41575ede00449c6ec008
이혜진 <annie_1229@naver.com>
2022-07-18 18:19:53 +0900

    add callback log

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,14 +11,14 @@ export default NextAuth({
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
-      console.log('[api/auth] jwt callback');
+      console.log('[api/auth] jwt callback', token, account);
       if (account) {
         token.accessToken = account.access_token
       }
       return token
     },
     async session({ session, token, user }) {
-      console.log('[api/auth] session callback');
+      console.log('[api/auth] session callback', session, token, user);
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken
       return session
